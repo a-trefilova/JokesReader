3383b4853ca0433af3853e7be0f743c1418eae81
Alyona Sabitskaya <trefilovaalyona@gmail.com>
2021-01-20 11:55:32 +0300

    Initial Commit

---
+++ ChuckNorrisJockes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChuckNorrisJockes
+//
+//  Created by Alyona Sabitskaya  on 20.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
